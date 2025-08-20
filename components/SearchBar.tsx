5af5b137c00b9237cc32dbd58f4b81a7a4f0f987
Chouaib-Djerdi <djerdichouaib@gmail.com>
2025-08-20 17:51:52 +0100

    feat: Add search bar, API integration Custom Hook

---
+++ components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+  onPress?: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 px-5 py-4 rounded-full">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain" // keep proportions inside container
+        tintColor="#ab8bff" // recolor icon purple
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
