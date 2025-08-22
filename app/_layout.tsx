f74e45f3b0ea0e5c831427ba13d6c369b1c07032
Chouaib-Djerdi <djerdichouaib@gmail.com>
2025-08-22 12:03:04 +0100

    feat: Appwrite integration and TrendingCard component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,21 @@
+import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import "./globals.css";
 
 export default function RootLayout() {
+  useEffect(() => {
+    // Change Android navigation bar color
+    NavigationBar.setBackgroundColorAsync("#030014"); // your color
+  }, []);
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
